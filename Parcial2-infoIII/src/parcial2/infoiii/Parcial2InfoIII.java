960662c0d2222935f60a388f582c3ac700d1f4a2
Ramiro <razcona390@alumnos.iua.edu.ar>
2017-10-31 10:54:56 -0300

    jeje

--- Parcial2-infoIII/src/parcial2/infoiii/Parcial2InfoIII.java
+++ Parcial2-infoIII/src/parcial2/infoiii/Parcial2InfoIII.java
@@ -32,7 +32,7 @@ public class Parcial2InfoIII extends Application {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String... args) {
         launch(args);
     }
     
